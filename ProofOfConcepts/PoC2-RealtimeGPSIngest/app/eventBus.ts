c33c8fdcdbc93a41918fad75e06d43abcb90a03c
Benjamin De Loore <s143999@ap.be>
2026-05-13 20:31:10 +0200

    Add eventbus from

---
+++ ProofOfConcepts/PoC2-RealtimeGPSIngest/app/eventBus.ts
@@ -0,0 +1,18 @@
+/**
+ * Een hele simpele event bus implementatie om de hoofd-PoC te ondersteunen
+ * Voor de volledige uitwerking bekijk PoC3-InterneEventBus
+ */
+type Handler = (payload: any) => void;
+
+export class EventBus {
+  private listeners: Map<string, Handler[]> = new Map();
+
+  subscribe(event: string, handler: Handler) {
+    if (!this.listeners.has(event)) this.listeners.set(event, []);
+    this.listeners.get(event)!.push(handler);
+  }
+
+  publish(event: string, payload: any) {
+    this.listeners.get(event)?.forEach((h) => h(payload));
+  }
+}
